2a7946b35537945826bca2ccc8706330581aad99
Irene Scott <irenejinscott@gmail.com>
2020-05-11 23:32:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ def monopoly_with_second_tier
 
 
 def monopoly_with_third_tier{
-		
+	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!	
 }
 end
 
